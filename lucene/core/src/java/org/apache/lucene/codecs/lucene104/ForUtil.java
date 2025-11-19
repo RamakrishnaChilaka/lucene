a9f7b374493c668c988de2c824f311c02e5207bb
Ramakrishna Chilaka <ramachil@amazon.com>
2025-11-19 23:01:55 +0530

    bulk io in outputstream index output

--- lucene/core/src/java/org/apache/lucene/codecs/lucene104/ForUtil.java
+++ lucene/core/src/java/org/apache/lucene/codecs/lucene104/ForUtil.java
@@ -150,9 +150,7 @@ public final class ForUtil {
       }
     }
 
-    for (int i = 0; i < numIntsPerShift; ++i) {
-      out.writeInt(tmp[i]);
-    }
+    out.writeInts(tmp, 0, numIntsPerShift);
   }
 
   /** Number of bytes required to encode 256 integers of {@code bitsPerValue} bits per value. */
